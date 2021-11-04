d25b44fecac9c14da4b7aa308f0848566969d405
Thomas Gull <thomas.gull@angela-bruderer.ch>
2021-11-04 15:24:29 +0100

    init new project with basic structur and folders.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="de">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="stylesheet" href="css/style.css">
+	<title>Adventskalender</title>
+</head>
+<body>
+	<div class="wrapper">
+		
+	</div>
+</body>
+</html>
